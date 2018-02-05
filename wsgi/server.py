2b0aed3a57f1efc7c89cae48d398f614f72b023f
pengpeng <351578076@qq.com>
2018-02-05 13:23:56 +0800

    second

--- wsgi/server.py
+++ wsgi/server.py
@@ -1,7 +1,7 @@
 __author__ = 'mocy'
 #coding:UTF-8
 from wsgiref.simple_server import make_server
-# 自己编写的application函数:
+# 自己编写的application()函数:
 def application(environ, start_response):
     start_response('200 OK', [('Content-Type', 'text/html')])
     body = r'<h1>Hello, %s!</h1>'%(environ['PATH_INFO'][1:] or 'web')
