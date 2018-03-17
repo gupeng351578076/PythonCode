d132354395c0a501490e81345000953fa814c3a2
pengpeng <351578076@qq.com>
2018-03-17 10:08:34 +0800

    three

--- oop/Student.py
+++ oop/Student.py
@@ -4,8 +4,10 @@ class Student(object):
     def __init__(self,name,score):
         self.__name = name#加上两个下划线表示该成员变量私有化
         self.__score = score
+
     def get_name(self):
         return self.__name
+
     def get_score(self):
         return self.__score
 
